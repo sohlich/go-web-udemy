ba74676d4132dd1234c21566b8657b98c6e0d178
Radek Sohlich <sohlich@gmail.com>
2017-01-27 05:09:36 +0100

    templates practice

---
+++ templates/main.go
@@ -0,0 +1,75 @@
+package main
+
+import "text/template"
+import "net/http"
+
+var (
+	tmpFunc = template.FuncMap{
+		"active":   activeOnly,
+		"duration": countDuration,
+	}
+	tpl = template.Must(template.New("").Funcs(tmpFunc).ParseGlob("gohtml/*"))
+)
+
+type UserData struct {
+	Name     string
+	UserName string
+}
+
+type Task struct {
+	Name   string
+	Status string
+	Events []Event
+}
+
+type Event struct {
+	Duration int64
+	Type     string
+}
+
+func main() {
+
+	server := http.NewServeMux()
+	server.HandleFunc("/", func(rw http.ResponseWriter, req *http.Request) {
+
+		data := struct {
+			User  UserData
+			Tasks []Task
+		}{
+			UserData{
+				"Radomir Sohlich",
+				"sola_cz",
+			},
+			[]Task{
+				Task{
+					Name: "Working",
+				},
+				Task{
+					Name: "Reading",
+				},
+			},
+		}
+
+		tpl.ExecuteTemplate(rw, "Index", data)
+	})
+	http.ListenAndServe(":8080", server)
+
+}
+
+func activeOnly(tasks []Task) Task {
+	for _, val := range tasks {
+		if val.Status == "running" {
+			return val
+		}
+	}
+
+	return Task{}
+}
+
+func countDuration(t Task) int64 {
+	total := int64(0)
+	for _, val := range t.Events {
+		total += val.Duration
+	}
+	return total
+}
