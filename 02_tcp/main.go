eb09a20625d98a1237668753c00c06487d476904
Radek Sohlich <sohlich@gmail.com>
2017-01-28 11:21:33 +0100

    simple tcp server

--- 02_tcp/main.go
+++ 02_tcp/main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"strings"
+)
 
+func main() {
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		panic("Error occured during listener creation" + err.Error())
+	}
+	conn, err := listener.Accept()
+	log.Println("Received connection")
+	if err != nil {
+		panic("Error occured")
+	}
+	log.Println("Connection going to be handled")
+	handle(conn)
+}
+
+func handle(conn net.Conn) {
+	log.Println("Connection being handled")
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+		io.Copy(conn, strings.NewReader("OK"))
+	}
 }
