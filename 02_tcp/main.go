7cfd8582d6652b713ca92fef37d36a54a7e13f77
Radek Sohlich <sohlich@gmail.com>
2017-01-29 05:09:27 +0100

    simple http request handling

--- 02_tcp/main.go
+++ 02_tcp/main.go
@@ -3,41 +3,49 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io"
 	"log"
 	"net"
-	"os"
-	"os/signal"
-	"strings"
-	"syscall"
 )
 
 func main() {
-	listener, err := net.Listen("tcp", ":8080")
+	l, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		panic("Error occured during listener creation" + err.Error())
 	}
-	conn, err := listener.Accept()
-	log.Println("Received connection")
-	if err != nil {
-		panic("Error occured")
+	defer l.Close()
+
+	for {
+		conn, err := l.Accept()
+		log.Println("Received connection")
+		if err != nil {
+			panic("Error occured")
+		}
+		log.Println("Connection going to be handled")
+		go handle(conn)
 	}
-	log.Println("Connection going to be handled")
-	go handle(conn)
-
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
-
-	<-signalChan
-	fmt.Println("Captured %v. Exiting...")
-	os.Exit(0)
 }
 
+var response = `
+<html>\r\n
+<body>\r\n
+<h1>Hello, World!</h1>\r\n
+</body>\r\n
+</html>\r\n`
+
 func handle(conn net.Conn) {
+	defer conn.Close()
 	log.Println("Connection being handled")
 	scanner := bufio.NewScanner(conn)
 	for scanner.Scan() {
 		fmt.Println(scanner.Text())
-		io.Copy(conn, strings.NewReader("OK"))
+		if scanner.Text() == "" {
+			break
+		}
 	}
+	fmt.Fprint(conn, "HTTP/1.1 200 OK\n")
+	fmt.Fprintf(conn, "Content-Length: %d\n", len(response))
+	fmt.Fprint(conn, "Content-Type: text/html\n")
+	fmt.Fprint(conn, "\n")
+	fmt.Fprint(conn, response)
+
 }
