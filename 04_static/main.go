786dae92f345a382b29d91efa20f16f648efe4fe
Radek Sohlich <sohlich@gmail.com>
2017-01-31 05:08:03 +0100

    more examples from http package

--- 04_static/main.go
+++ 04_static/main.go
@@ -4,6 +4,8 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"strings"
+	"time"
 )
 
 var (
@@ -11,11 +13,23 @@ var (
 )
 
 func main() {
-	http.Handle("/resources/*", http.StripPrefix("/resources", http.FileServer(http.Dir("./assets"))))
+	http.HandleFunc("/special/data.dat", specialFileHandler)
+	http.HandleFunc("/special/", specialHandler)
+	http.Handle("/resources/", http.StripPrefix("/resources", http.FileServer(http.Dir("./assets"))))
 	http.HandleFunc("/", indexHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+}
+
+func specialHandler(w http.ResponseWriter, r *http.Request) {
+	// Serve specific file or directory
+	http.ServeFile(w, r, "./special/golden.jpg") // Serves file
+}
+
+func specialFileHandler(w http.ResponseWriter, r *http.Request) {
+	// Serve content needs all data for the content
+	http.ServeContent(w, r, "data", time.Now(), strings.NewReader("Hello"))
 }
