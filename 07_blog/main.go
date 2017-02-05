e6f883017439696768bf0c6d8c7638210ed1b93f
Radek Sohlich <sohlich@gmail.com>
2017-02-05 05:36:33 +0100

    image gallery and upload

--- 07_blog/main.go
+++ 07_blog/main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"html/template"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	"path/filepath"
 
 	"github.com/goadesign/goa/uuid"
 )
@@ -18,7 +21,7 @@ func main() {
 
 	http.HandleFunc("/login", login)
 	http.HandleFunc("/home", secure(home))
-
+	http.Handle("/image/", http.StripPrefix("/image", http.FileServer(http.Dir("./storage"))))
 	http.ListenAndServe(":8080", nil)
 
 }
@@ -52,45 +55,67 @@ func login(w http.ResponseWriter, r *http.Request) {
 }
 
 type Picture struct {
+	Name string
+	URL  string
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
 	log.Println("serving: Home")
 	c, _ := r.Cookie("auth")
 
+	var errString string
 	if r.Method == http.MethodPost {
-		f, fh, e := r.FormFile("photo")
-		defer f.Close()
-		if e != nil {
-			log.Println("cannot read file from form: " + e.Error())
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		os.MkdirAll("./storage/"+c.Value, os.ModePerm)
-		outF, e := os.OpenFile("./storage/"+c.Value+"/"+fh.Filename, os.O_CREATE|os.O_WRONLY, os.ModePerm)
-		defer outF.Close()
-		if e != nil {
-			log.Println("cannot open file: " + e.Error())
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		if _, e = io.Copy(outF, f); e != nil {
-			log.Println("cannot write file: " + e.Error())
-			w.WriteHeader(http.StatusInternalServerError)
-			return
+		if err := homePost(r, c.Value); err != nil {
+			errString = err.Error()
 		}
 	}
+
+	pictures := listFiles(c.Value)
 
 	data := struct {
 		UserName string
 		Pictures []Picture
+		Error    string
 	}{
 		c.Value,
-		make([]Picture, 0),
+		pictures,
+		errString,
 	}
 	if err := tpl.ExecuteTemplate(w, "home.gohtml", data); err != nil {
 		log.Println("err serving home: ", err.Error())
 		return
 	}
+}
+
+func homePost(r *http.Request, userid string) error {
+	f, fh, e := r.FormFile("photo")
+	defer f.Close()
+	if e != nil {
+		return e
+	}
+
+	os.MkdirAll("./storage/"+userid, os.ModePerm)
+	outF, e := os.OpenFile("./storage/"+userid+"/"+fh.Filename, os.O_CREATE|os.O_WRONLY, os.ModePerm)
+	defer outF.Close()
+	if e != nil {
+		return e
+	}
+	if _, e = io.Copy(outF, f); e != nil {
+		return e
+	}
+	return nil
+}
+
+func listFiles(path string) []Picture {
+	files, err := ioutil.ReadDir("./storage/" + path)
+	if err != nil {
+		return make([]Picture, 0)
+	}
+
+	paths := make([]Picture, len(files))
+	for idx, file := range files {
+		paths[idx].Name = file.Name()
+		paths[idx].URL = filepath.Join("/image", path, file.Name())
+	}
+	return paths
 }
